e5592ad19f3578f494771ffe4553a8ab3dd5eb84
Robson Batista Solano <55255033+RobsonSolano@users.noreply.github.com>
2020-10-05 21:49:04 -0300

    Projeto preparado

--- views-cache/footer.3a3e24af2303d41f6c90c499af75f26e.rtpl.php
+++ views-cache/footer.3a3e24af2303d41f6c90c499af75f26e.rtpl.php
@@ -1,12 +1,21 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?><!-- Main Footer -->
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        Desafio Nano - Controle geral
+<footer class="bg-light fixed-bottom border-top p-3">
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                <h4 class="border-bottom">Dashboard - Desafio nano</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12  d-flex justify-content-center">
+                <strong>Copyright &copy; 2020 <a href="/">Company</a>.</strong> Todos os direitos reservados.
+            </div>
+        </div>
     </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2020 <a href="#">Empresa</a>.</strong> Todos direitos reservados.
+
 </footer>
+
 </body>
 
 </html>
--- views-cache/header.3a3e24af2303d41f6c90c499af75f26e.rtpl.php
+++ views-cache/header.3a3e24af2303d41f6c90c499af75f26e.rtpl.php
@@ -9,34 +9,67 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1,
             user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="/res/css/main.css">
 
 </head>
 
+<script>
+    $(document).ready(function() {
+        console.log("document is ready");
+        $('[data-toggle="offcanvas"], #navToggle').on('click', function() {
+            $('.offcanvas-collapse').toggleClass('open')
+        })
+    });
+    window.onload = function() {
+        console.log("window is loaded");
+    };
+</script>
+
 <body>
 
+    <header>
+        <!--Cabeçalho-->
+        <nav class="navbar navbar-expand-lg fixed-top shadow navbar-dark navbar-offcanvas bg-dark">
+            <a class="navbar-brand mr-auto" href="/">DASHBOARD</a>
 
+            <button class="navbar-toggler d-block" type="button" id="navToggle">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse offcanvas-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="logutAdmin" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <strong>Nome Adminin</strong>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="logutAdmin">
+                            <a class="dropdown-item text-white" href="/admin/perfil/">Perfil</a>
+                            <a class="dropdown-item text-white" href="/admin/logout/">Sair</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/funcionarios/">Funcionários</a>
+                    </li>
 
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/analytics/">Analytics</a>
+                    </li>
+                    <li class="nav-item nav-item-final">
+                        <a class="nav-link">Developer</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--Fim do cabeçalho-->
 
-    <!-- Sidebar Menu -->
-    <ul class="sidebar-menu">
-        <li class="header">HEADER</li>
-        <!-- Optionally, you can add icons to the links -->
-        <li><a href="/admin/users"><i class="fa fa-users"></i>
-                            <span>Usuários</span></a></li>
-        <li><a href="/admin/categories"><i class="fa fa-link"></i>
-                            <span>Categorias</span></a></li>
-        <li><a href="/admin/products"><i class="fa fa-link"></i>
-                            <span>Produtos</span></a></li>
-        <li><a href="/admin/orders"><i class="fa fa-shopping-cart"></i>
-                            <span>Pedidos</span></a></li>
-
-
-    </ul>
+    </header>
